51aaa97bc5121773476349be1e277262a424ce96
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-01 18:25:05 -0300

    feat: add AppSidebar component with collapsible menu and user profile integration

---
+++ src/components/layout/sidebar.component.tsx
@@ -0,0 +1,223 @@
+import { NavLink, useLocation } from "react-router";
+import * as React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Separator } from "@/components/ui/separator";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarProvider,
+  SidebarRail,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import {
+  ChevronRight,
+  ChevronsUpDown,
+  GalleryVerticalEnd,
+  LogOut,
+} from "lucide-react";
+import { ThemeToggle } from "@/components/layout/theme-toggle.component";
+
+const company = {
+  name: "Acme Inc",
+  logo: GalleryVerticalEnd,
+  plan: "Enterprise",
+};
+
+export interface SidebarItem {
+  title: string;
+  url: string;
+  icon: React.ElementType;
+  isActive?: boolean;
+  children?: SidebarItem[];
+}
+
+interface SidebarProps {
+  data: SidebarItem[];
+  children: React.ReactNode;
+}
+
+export function AppSidebar({ data, children }: SidebarProps) {
+  const [mounted, setMounted] = React.useState(false);
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null; // or a loading skeleton
+  }
+
+  return (
+    <SidebarProvider>
+      <Sidebar collapsible="icon">
+        <SidebarHeader>
+          <div className="flex gap-2 py-2 text-sidebar-accent-foreground ">
+            <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+              <company.logo className="size-4" />
+            </div>
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <span className="truncate font-semibold">{company.name}</span>
+              <span className="truncate text-xs">{company.plan}</span>
+            </div>
+          </div>
+        </SidebarHeader>
+        <SidebarContent className="overflow-x-hidden">
+          <SidebarGroup>
+            <SidebarMenu>
+              {data.map(({ title, url, icon: Icon, isActive, children }) => {
+                return children && children?.length > 0 ? (
+                  <Collapsible
+                    key={title}
+                    asChild
+                    defaultOpen={isActive}
+                    className="group/collapsible"
+                  >
+                    <SidebarMenuItem>
+                      <CollapsibleTrigger asChild>
+                        <SidebarMenuButton
+                          tooltip={title}
+                          isActive={pathname === url}
+                        >
+                          <Icon />
+                          <span>{title}</span>
+                          <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                        </SidebarMenuButton>
+                      </CollapsibleTrigger>
+                      <CollapsibleContent>
+                        <SidebarMenuSub>
+                          {children?.map((subItem) => (
+                            <SidebarMenuSubItem key={subItem.title}>
+                              <SidebarMenuSubButton
+                                asChild
+                                isActive={pathname === subItem.url}
+                              >
+                                <NavLink to={subItem.url}>
+                                  <span>{subItem.title}</span>
+                                </NavLink>
+                              </SidebarMenuSubButton>
+                            </SidebarMenuSubItem>
+                          ))}
+                        </SidebarMenuSub>
+                      </CollapsibleContent>
+                    </SidebarMenuItem>
+                  </Collapsible>
+                ) : (
+                  <SidebarMenuItem key={title}>
+                    <SidebarMenuButton
+                      asChild
+                      tooltip={title}
+                      isActive={pathname === url}
+                    >
+                      <NavLink to={url}>
+                        <Icon />
+                        <span>{title}</span>
+                      </NavLink>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton
+                    size="lg"
+                    className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                  >
+                    <Avatar className="h-8 w-8 rounded-lg">
+                      <AvatarImage
+                        src="https://gravatar.com/avatar/6adb9ad2fda8bc8115dda6864f8c768f?s=400&d=robohash&r=x"
+                        alt="User Name"
+                      />
+                      <AvatarFallback className="rounded-lg">
+                        {"CN"}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="grid flex-1 text-left text-sm leading-tight">
+                      <span className="truncate font-semibold">User Name</span>
+                      <span className="truncate text-xs">User Email</span>
+                    </div>
+                    <ChevronsUpDown className="ml-auto size-4" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                  side="bottom"
+                  align="end"
+                  sideOffset={4}
+                >
+                  <DropdownMenuLabel className="p-0 font-normal">
+                    <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                      <Avatar className="h-8 w-8 rounded-lg">
+                        <AvatarImage
+                          src="https://gravatar.com/avatar/6adb9ad2fda8bc8115dda6864f8c768f?s=400&d=robohash&r=x"
+                          alt="User Name"
+                        />
+                        <AvatarFallback className="rounded-lg">
+                          {"CN"}
+                        </AvatarFallback>
+                      </Avatar>
+                      <div className="grid flex-1 text-left text-sm leading-tight">
+                        <span className="truncate font-semibold">
+                          User Name
+                        </span>
+                        <span className="truncate text-xs">User Email</span>
+                      </div>
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <LogOut />
+                    Log out
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+        <SidebarRail />
+      </Sidebar>
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-2 sm:px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+          </div>
+
+          <div className="flex items-center gap-2 px-2 sm:px-4">
+            <ThemeToggle />
+          </div>
+        </header>
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
