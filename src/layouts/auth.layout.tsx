64ca7081b825743e29386495b433fbff5845f277
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-05 08:35:09 -0300

    feat: enhance AuthLayout with centered styling for better user experience

--- src/layouts/auth.layout.tsx
+++ src/layouts/auth.layout.tsx
@@ -1,5 +1,12 @@
 import { Outlet } from "react-router";
 
 export function AuthLayout() {
-  return <Outlet />;
+  return (
+    <div
+      className="flex items-center justify-center h-dvh bg-gray-50"
+      style={{ minHeight: "100vh" }}
+    >
+      <Outlet />;
+    </div>
+  );
 }
