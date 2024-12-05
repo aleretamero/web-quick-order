40de068344b947d442a9497b82e874a9c8abfdc6
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-05 17:59:45 -0300

    feat: add custom hook for fetching completed orders report with date range

---
+++ src/domain/report/hooks/use-orders-completed-report.hook.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { getOrdersCompletedReport } from "@/domain/report/actions/get-orders-completed-report.action";
+import { useQuryParamsDateRange } from "@/hooks/use-query-params-data-range.hook";
+
+export function useGetOrdersCompletedReport() {
+  const { initialDate, finalDate } = useQuryParamsDateRange();
+  return useQuery({
+    queryKey: [`/reports//reports/orders-completed`, initialDate, finalDate],
+    queryFn: () => {
+      if (!initialDate || !finalDate) {
+        return null;
+      }
+
+      return getOrdersCompletedReport({
+        from: initialDate,
+        to: finalDate,
+      }).then((response) => response.data);
+    },
+    initialData: null,
+  });
+}
