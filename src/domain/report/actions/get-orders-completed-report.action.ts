c695840d8ddb7eb3890e40a13674155c4fbf3bcd
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-05 17:59:39 -0300

    feat: add getOrdersCompletedReport action for fetching completed orders report

---
+++ src/domain/report/actions/get-orders-completed-report.action.ts
@@ -0,0 +1,18 @@
+import { HttpResponse } from "@/lib/http/http-response";
+import { apiClient } from "@/services/api/api-service";
+import { OrdersCompletedReportModel } from "@/domain/report/models/orders-completed-report.model";
+import { DateRangeQuery } from "@/services/api/api-types";
+
+export async function getOrdersCompletedReport(
+  query: DateRangeQuery
+): Promise<HttpResponse<OrdersCompletedReportModel>> {
+  return apiClient.get<OrdersCompletedReportModel>(
+    `/reports/orders-completed`,
+    {
+      params: {
+        from: query.from.toISOString().split("T")[0],
+        to: query.to.toISOString().split("T")[0],
+      },
+    }
+  );
+}
