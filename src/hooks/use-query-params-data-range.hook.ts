6299b56b81cb8a30562ccec4245a268d000b7cf7
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-05 08:29:05 -0300

    feat: add useQueryParamsDateRange hook for managing date range in query parameters

---
+++ src/hooks/use-query-params-data-range.hook.ts
@@ -0,0 +1,33 @@
+import { useCallback, useMemo } from "react";
+import { parseAsIsoDate, useQueryState } from "nuqs";
+
+export function useQuryParamsDateRange() {
+  const defaultInitialDate = useMemo(() => null, []);
+  const initialDateKey = useMemo(() => "initial_date", []);
+  const [initialDate, setInitialDate] = useQueryState(
+    initialDateKey,
+    parseAsIsoDate.withOptions({ clearOnDefault: true })
+  );
+
+  const defaultFinalDate = useMemo(() => null, []);
+  const finalDateKey = useMemo(() => "final_date", []);
+  const [finalDate, setFinalDate] = useQueryState(
+    finalDateKey,
+    parseAsIsoDate.withOptions({ clearOnDefault: true })
+  );
+
+  const resetDateRange = useCallback(() => {
+    setInitialDate(defaultInitialDate);
+    setFinalDate(defaultFinalDate);
+  }, [defaultInitialDate, defaultFinalDate, setInitialDate, setFinalDate]);
+
+  return {
+    initialDate,
+    setInitialDate,
+    initialDateKey,
+    finalDate,
+    setFinalDate,
+    finalDateKey,
+    resetDateRange,
+  };
+}
