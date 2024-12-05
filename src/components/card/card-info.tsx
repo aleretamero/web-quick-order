9888f126095e28175c4c7d35ce695b84e15fee1e
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-05 12:57:29 -0300

    feat: update CardInfo component to support multiple descriptions

--- src/components/card/card-info.tsx
+++ src/components/card/card-info.tsx
@@ -4,11 +4,15 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 interface CardInfoProps {
   title: string;
   value: string;
-  description?: string;
+  descriptions?: string[] | string;
   icon?: ReactElement;
 }
 
-export function CardInfo({ title, value, description, icon }: CardInfoProps) {
+export function CardInfo({ title, value, descriptions, icon }: CardInfoProps) {
+  if (typeof descriptions === "string") {
+    descriptions = [descriptions];
+  }
+
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -16,10 +20,15 @@ export function CardInfo({ title, value, description, icon }: CardInfoProps) {
         {icon}
       </CardHeader>
       <CardContent>
-        <div className="text-2xl font-bold">{value}</div>
-        {description && (
-          <p className="text-xs text-muted-foreground">{description}</p>
-        )}
+        <div className="text-2xl font-bold mb-2">{value}</div>
+        {descriptions?.map((description, index) => (
+          <p
+            key={`${title}-description-${index}`}
+            className="text-xs text-muted-foreground"
+          >
+            {description}
+          </p>
+        ))}
       </CardContent>
     </Card>
   );
