26039d0752d7d1a33172031e425f0ff0621f9614
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-07 13:17:06 -0300

    feat: add InputDatePickerForm component for date selection in forms

---
+++ src/components/form/input-date-picker-form.component.tsx
@@ -0,0 +1,76 @@
+import { FieldValues, Path, UseFormReturn } from "react-hook-form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { CalendarIcon } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+interface InputDatePickerFormProps<T extends FieldValues> {
+  name: Path<T>;
+  form: UseFormReturn<T>;
+  label?: string;
+  className?: string;
+}
+
+export const InputDatePickerForm = <T extends FieldValues>({
+  name,
+  form,
+  label,
+  className,
+}: InputDatePickerFormProps<T>) => {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className={cn("flex flex-col", className)}>
+          {label && <FormLabel>{label}</FormLabel>}
+          <Popover>
+            <PopoverTrigger asChild>
+              <FormControl>
+                <Button
+                  variant={"outline"}
+                  className={cn(
+                    "w-[240px] pl-3 text-left font-normal",
+                    !field.value && "text-muted-foreground"
+                  )}
+                >
+                  {field.value ? (
+                    format(field.value, "PPP", { locale: ptBR })
+                  ) : (
+                    <span>Selecione uma data</span>
+                  )}
+                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                mode="single"
+                selected={field.value}
+                onSelect={field.onChange}
+                disabled={false}
+                initialFocus
+              />
+            </PopoverContent>
+          </Popover>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+InputDatePickerForm.displayName = "InputDatePickerForm";
