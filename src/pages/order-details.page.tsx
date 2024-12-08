465e0a5711762a98e150d804370cbbc2eda10fd7
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-08 13:55:33 -0300

    feat: implement OrderDetailsPage to display order details using useGetOrder hook

--- src/pages/order-details.page.tsx
+++ src/pages/order-details.page.tsx
@@ -1,3 +1,15 @@
+import { PageContainer } from "@/components/layout/page-container.component";
+import { useGetOrder } from "@/domain/orders/hooks/get-order.hook";
+import { useParams } from "react-router";
+
 export function OrderDetailsPage() {
-  return <h1>OrderDetailsPage</h1>;
+  const { id } = useParams();
+
+  const { data } = useGetOrder(id);
+
+  return (
+    <PageContainer scrollable>
+      <pre>{JSON.stringify(data, null, 2)}</pre>;
+    </PageContainer>
+  );
 }
