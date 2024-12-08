1efec4662122fce112b9f2128c2a75644d4a26f6
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-08 12:33:08 -0300

    feat: update OrdersFilters to use dynamic status key and exclude deleted status

--- src/components/app/orders/orders-filters.component.tsx
+++ src/components/app/orders/orders-filters.component.tsx
@@ -1,17 +1,22 @@
 import { BoxMultipleFilter } from "@/components/filter/box-multiple-filter.component";
 import { DateRangePickerFilter } from "@/components/filter/date-range-picker-filter.component";
 import { OrderStatus } from "@/domain/orders/enums/order-status.enum";
+import { useQueryParamsOrderStatus } from "@/domain/orders/hooks/use-query-params-order-status";
 
 export function OrdersFilters() {
+  const { key } = useQueryParamsOrderStatus();
+
   return (
     <div className="flex justify-between items-center gap-4">
       <BoxMultipleFilter
         title="Filtros"
-        filterKey={"tableStatusKey"}
-        options={Object.entries(OrderStatus).map(([key, value]): any => ({
-          value: key,
-          label: value,
-        }))}
+        filterKey={key}
+        options={Object.entries(OrderStatus)
+          .filter(([key]) => key !== OrderStatus.DELETED)
+          .map(([key, value]): any => ({
+            value: key,
+            label: value,
+          }))}
         align="start"
         size="lg"
       />
