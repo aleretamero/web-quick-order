a012b3e59e88d0ccd3e37c3532843e02266dee2a
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-08 12:54:58 -0300

    feat: update getOrdersAction to include status and date range parameters

--- src/domain/orders/actions/get-orders.action.ts
+++ src/domain/orders/actions/get-orders.action.ts
@@ -1,23 +1,30 @@
 import { HttpResponse } from "@/lib/http/http-response";
 import { apiClient } from "@/services/api/api-service";
 import { OrderModel } from "@/domain/orders/models/order.model";
-import { PaginationResponse } from "@/services/api/api-types";
+import { DateRangeQuery, PaginationResponse } from "@/services/api/api-types";
 
 type GetOrdersActionParams = {
   page: number;
   limit: number;
-};
+  status: string[];
+} & DateRangeQuery;
 
-export async function getOrdersAction({
-  limit,
-  page,
-}: GetOrdersActionParams): Promise<
-  HttpResponse<PaginationResponse<OrderModel>>
-> {
+export async function getOrdersAction(
+  query: GetOrdersActionParams
+): Promise<HttpResponse<PaginationResponse<OrderModel>>> {
+
+
+  const params = new URLSearchParams();
+
+  params.append('limit', query.limit.toString());
+  params.append('page', query.page.toString());
+  query.status.forEach((status) => params.append('status', status));
+  params.append('from', query.from.toISOString().split("T")[0]);
+  params.append('to', query.to.toISOString().split("T")[0]);
+
+
+
   return apiClient.get<PaginationResponse<OrderModel>>(`/orders`, {
-    params: {
-      limit,
-      page,
-    },
+    params,
   });
 }
