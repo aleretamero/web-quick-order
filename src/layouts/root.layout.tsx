b583355d5ddefcaf26e719827e762a09947fe49d
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-08 13:01:18 -0300

    feat: wrap ThemeContext with ThemeProvider in RootLayout component

--- src/layouts/root.layout.tsx
+++ src/layouts/root.layout.tsx
@@ -1,18 +1,20 @@
 import { AuthProvider } from "@/contexts/auth.context";
-import { ThemeContext } from "@/contexts/theme.context";
+import { ThemeContext, ThemeProvider } from "@/contexts/theme.context";
 import { cn } from "@/lib/utils";
 import { Outlet } from "react-router";
 
 export function RootLayout() {
   return (
     <AuthProvider>
-      <ThemeContext.Consumer>
-        {({ theme }) => (
-          <div className={cn("w-full", theme)}>
-            <Outlet />
-          </div>
-        )}
-      </ThemeContext.Consumer>
+      <ThemeProvider>
+        <ThemeContext.Consumer>
+          {({ theme }) => (
+            <div className={cn("w-full", theme)}>
+              <Outlet />
+            </div>
+          )}
+        </ThemeContext.Consumer>
+      </ThemeProvider>
     </AuthProvider>
   );
 }
