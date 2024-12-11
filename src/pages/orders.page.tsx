68fea24ca65e5815d22b2df657a20fb6ffcf6420
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-11 10:57:14 -0300

    feat: integrate ModalSaveOrder into OrdersPage layout for improved order management

--- src/pages/orders.page.tsx
+++ src/pages/orders.page.tsx
@@ -1,12 +1,16 @@
 import { OrdersFilters } from "@/components/app/orders/orders-filters.component";
 import { OrdersTable } from "@/components/app/orders/orders-table.component";
+import { ModalSaveOrder } from "@/components/app/orders/save-order-modal.component";
 import { PageContainer } from "@/components/layout/page-container.component";
 
 export function OrdersPage() {
   return (
     <PageContainer scrollable>
       <div className="flex flex-col gap-4 h-full">
-        <OrdersFilters />
+        <div className="flex justify-between items-center gap-4">
+          <OrdersFilters />
+          <ModalSaveOrder />
+        </div>
         <OrdersTable />
       </div>
     </PageContainer>
