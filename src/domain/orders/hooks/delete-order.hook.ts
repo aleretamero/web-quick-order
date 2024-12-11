f1aa255a3e9049c8361c7e57c411e14cc77809f1
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-10 23:07:10 -0300

    feat: implement useDeleteOrder hook with success and error handling

--- src/domain/orders/hooks/delete-order.hook.ts
+++ src/domain/orders/hooks/delete-order.hook.ts
@@ -1,9 +1,23 @@
 import { deleteOrderAction } from "@/domain/orders/actions/delete-order.action";
-import { useMutation } from "@tanstack/react-query";
+import { useToast } from '@/hooks/use-toast.hook';
+import { HttpError } from '@/lib/http/http-error';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeleteOrder() {
+  const queryClient = useQueryClient();
+  const toast = useToast();
+
   return useMutation({
     mutationFn: (id: string) =>
       deleteOrderAction(id).then((response) => response),
+    onSuccess: (_, id) => {
+      queryClient.invalidateQueries({
+        queryKey: ["/orders", id],
+      });
+      toast.success("Pedido removido com sucesso");
+    },
+    onError: (error: HttpError) => {
+      toast.error(error.data.errors);
+    },
   });
 }
