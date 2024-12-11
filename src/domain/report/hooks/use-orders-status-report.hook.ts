f86e46e571586a48c107698a9b1733b3cc53b221
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-11 14:37:04 -0300

    feat: integrate setDefaultDateRange in useGetOrdersStatusReport to handle undefined date ranges

--- src/domain/report/hooks/use-orders-status-report.hook.ts
+++ src/domain/report/hooks/use-orders-status-report.hook.ts
@@ -1,9 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { useQueryParamsDateRange } from "@/hooks/use-query-params-data-range.hook";
 import { getOrdersStatusReport } from "@/domain/report/actions/get-orders-status-report.action";
+import { useEffect } from "react";
 
 export function useGetOrdersStatusReport() {
-  const { initialDate, finalDate } = useQueryParamsDateRange();
+  const { initialDate, finalDate, setDefaultDateRange } =
+    useQueryParamsDateRange();
+
+  useEffect(() => {
+    if (!initialDate || !finalDate) setDefaultDateRange();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return useQuery({
     queryKey: [`/reports/reports/orders-status`, initialDate, finalDate],
     queryFn: () => {
