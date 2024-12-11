4a43892cc614f6414cbd39639677870cdde1a445
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-11 09:49:40 -0300

    refactor: simplify EditButtonIcon props to extend button attributes

--- src/components/button/edit-button.tsx
+++ src/components/button/edit-button.tsx
@@ -1,13 +1,12 @@
+import { ComponentPropsWithRef } from "react";
 import { Edit } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-interface TrashButtonIconProps {
-  onClick?: () => void;
-}
+interface TrashButtonIconProps extends ComponentPropsWithRef<"button"> {}
 
-export function EditButtonIcon({ onClick }: TrashButtonIconProps) {
+export function EditButtonIcon(props: TrashButtonIconProps) {
   return (
-    <Button variant="outline" size="icon" onClick={onClick}>
+    <Button variant="outline" size="icon" {...props}>
       <Edit />
     </Button>
   );
