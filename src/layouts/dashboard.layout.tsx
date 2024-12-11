32707a4ed77a65652adcd3bc8138da608cc8537e
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-11 10:18:20 -0300

    feat: conditionally render sidebar items based on user role in DashboardLayout

--- src/layouts/dashboard.layout.tsx
+++ src/layouts/dashboard.layout.tsx
@@ -1,26 +1,40 @@
 import { Outlet } from "react-router";
 import { LayoutDashboard, PlusCircleIcon, Table } from "lucide-react";
 import { AppSidebar, SidebarItem } from "@/components/layout/sidebar.component";
-
-const sidebarData: SidebarItem[] = [
-  {
-    title: "Início",
-    icon: LayoutDashboard,
-    url: "/home",
-  },
-  {
-    title: "Pedidos",
-    icon: Table,
-    url: "/orders",
-  },
-  {
-    title: "Novo",
-    icon: PlusCircleIcon,
-    url: "/orders/create",
-  },
-];
+import { useAuth } from "@/hooks/use-auth.hook";
+import { Role } from "@/domain/user/enums/role.enum";
 
 export function DashboardLayout() {
+  const { dataUser } = useAuth();
+
+  const sidebarData: SidebarItem[] = [
+    ...(dataUser?.role === Role.ADMIN
+      ? [
+          {
+            title: "Início",
+            icon: LayoutDashboard,
+            url: "/home",
+          },
+        ]
+      : []),
+
+    {
+      title: "Pedidos",
+      icon: Table,
+      url: "/orders",
+    },
+
+    ...(dataUser?.role === Role.ADMIN
+      ? [
+          {
+            title: "Novo",
+            icon: PlusCircleIcon,
+            url: "/orders/create",
+          },
+        ]
+      : []),
+  ];
+
   return (
     <AppSidebar data={sidebarData}>
       <Outlet />
