63c28cb453bedda9922d3854d0b95fd66f61642a
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-11 17:10:38 -0300

    feat: conditionally render OrdersTable or OrdersViewCards based on mobile view

--- src/pages/orders.page.tsx
+++ src/pages/orders.page.tsx
@@ -1,9 +1,12 @@
 import { OrdersFilters } from "@/components/app/orders/orders-filters.component";
 import { OrdersTable } from "@/components/app/orders/orders-table.component";
+import { OrdersViewCards } from "@/components/app/orders/orders-view-cards.component";
 import { ModalSaveOrder } from "@/components/app/orders/save-order-modal.component";
 import { PageContainer } from "@/components/layout/page-container.component";
+import { useIsMobile } from "@/hooks/use-is-mobile.hook";
 
 export function OrdersPage() {
+  const isMobile = useIsMobile();
   return (
     <PageContainer scrollable>
       <div className="flex flex-col gap-4 h-full">
@@ -11,7 +14,7 @@ export function OrdersPage() {
           <OrdersFilters />
           <ModalSaveOrder />
         </div>
-        <OrdersTable />
+        {!isMobile ? <OrdersTable /> : <OrdersViewCards />}
       </div>
     </PageContainer>
   );
