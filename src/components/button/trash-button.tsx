0ea02e58baa261411bfe1dd38cf21aea25e9a836
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-11 09:49:46 -0300

    refactor: update TrashButtonIcon props to extend button attributes

--- src/components/button/trash-button.tsx
+++ src/components/button/trash-button.tsx
@@ -1,13 +1,12 @@
+import { ComponentPropsWithRef } from "react";
 import { Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-interface TrashButtonIconProps {
-  onClick?: () => void;
-}
+interface TrashButtonIconProps extends ComponentPropsWithRef<"button"> {}
 
-export function TrashButtonIcon({ onClick }: TrashButtonIconProps) {
+export function TrashButtonIcon(props: TrashButtonIconProps) {
   return (
-    <Button variant="outline" size="icon" onClick={onClick}>
+    <Button variant="outline" size="icon" {...props}>
       <Trash2 />
     </Button>
   );
