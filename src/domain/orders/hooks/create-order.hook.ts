dd2b6f070dab04afa46de051349ed3116b64f3d3
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-10 23:07:05 -0300

    feat: enhance useCreateOrder hook with success and error handling

--- src/domain/orders/hooks/create-order.hook.ts
+++ src/domain/orders/hooks/create-order.hook.ts
@@ -1,12 +1,26 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import {
   createOrderAction,
   CreateOrderDto,
 } from "@/domain/orders/actions/create-order.action";
+import { HttpError } from '@/lib/http/http-error';
+import { useToast } from '@/hooks/use-toast.hook';
 
 export function useCreateOrder() {
+  const queryClient = useQueryClient();
+  const toast = useToast();
+
   return useMutation({
     mutationFn: (data: CreateOrderDto) =>
       createOrderAction(data).then((response) => response),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["/orders"],
+      });
+      toast.success("Pedido criado com sucesso");
+    },
+    onError: (error: HttpError) => {
+      toast.error(error.data.errors);
+    },
   });
 }
