f3b1bf3ab6727eb8bd5f8fcbd8be1a22bc39701d
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-11 10:55:43 -0300

    refactor: rename TrashButtonIconProps to EditButtonIconProps for clarity

--- src/components/button/edit-button.tsx
+++ src/components/button/edit-button.tsx
@@ -2,9 +2,9 @@ import { ComponentPropsWithRef } from "react";
 import { Edit } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-interface TrashButtonIconProps extends ComponentPropsWithRef<"button"> {}
+interface EditButtonIconProps extends ComponentPropsWithRef<"button"> {}
 
-export function EditButtonIcon(props: TrashButtonIconProps) {
+export function EditButtonIcon(props: EditButtonIconProps) {
   return (
     <Button variant="outline" size="icon" {...props}>
       <Edit />
