69bfa6afebca017a3fa2fb7280bf52d29a7317d5
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-11 10:57:07 -0300

    feat: add navigation to orders page after successful order deletion

--- src/domain/orders/hooks/delete-order.hook.ts
+++ src/domain/orders/hooks/delete-order.hook.ts
@@ -2,8 +2,10 @@ import { deleteOrderAction } from "@/domain/orders/actions/delete-order.action";
 import { useToast } from '@/hooks/use-toast.hook';
 import { HttpError } from '@/lib/http/http-error';
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from 'react-router';
 
 export function useDeleteOrder() {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const toast = useToast();
 
@@ -15,6 +17,7 @@ export function useDeleteOrder() {
         queryKey: ["/orders", id],
       });
       toast.success("Pedido removido com sucesso");
+      navigate("/orders")
     },
     onError: (error: HttpError) => {
       toast.error(error.data.errors);
